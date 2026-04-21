b123d57e7d4d8019c1c28df80c74d8e95baa0da9
ccc <15601628069@163.com>
2026-04-21 19:35:34 +0800

    实现pdf解析

--- src/rag/rag.controller.ts
+++ src/rag/rag.controller.ts
@@ -1,5 +1,4 @@
 import {
-  Body,
   Controller,
   Post,
   UploadedFile,
@@ -8,14 +7,14 @@ import {
 import { RagService } from './rag.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import 'multer';
+
 @Controller('rag')
 export class RagController {
   constructor(private readonly ragService: RagService) {}
 
-  //1.上传文件接口
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file')) // 字段名必须是 file
-  uploadPdf(@UploadedFile() file: Express.Multer.File) {
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadPdf(@UploadedFile() file: Express.Multer.File) {
     return this.ragService.upload(file);
   }
 }
--- src/rag/rag.service.ts
+++ src/rag/rag.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@nestjs/common';
+// 文件上传引入
+import * as fs from 'fs';
+const { PDFParse } = require('pdf-parse');
 
 @Injectable()
 export class RagService {
-  upload(file: any) {
-    console.log('上传的PDF文件：', file);
+  async upload(file: any) {
+    try {
+      //1,读取文件 buffer
+      const fileBuffer = fs.readFileSync(file.path);
+      // 2. 解析 PDF 文本
+      const parser = new PDFParse({ data: fileBuffer });
+      const pdfResult = await parser.getText();
+      //   console.log('文本', pdfResult.text);
+      //用完解析器之后直接销毁
+      await parser.destroy();
 
-    return {
-      success: true,
-      filePath: file.path,
-      fileName: file.originalname,
-      message: 'PDF 上传成功',
-    };
+      return {
+        success: true,
+        filePath: file.path,
+        fileName: file.originalname,
+        message: 'PDF 上传并解析成功',
+        pdfText: pdfResult.text, // 解析后的文本
+        pages: pdfResult.numpages, // 页数
+      };
+    } catch (e) {
+      console.error('PDF上传/解析失败，错误详情:', e);
+      return {
+        success: false,
+        message: 'PDF 上传失败',
+        error: e,
+      };
+    }
   }
 }
